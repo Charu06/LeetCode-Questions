9ba160a743c3f386e8e9ce85a1ed9f442c1bfb44
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-01-20 23:50:34 +0530

    Time: 4 ms (47.92%), Space: 43.1 MB (61.93%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -2,16 +2,16 @@ class Solution {
     public int[] decompressRLElist(int[] nums) {
         int l=nums.length;
         int c=0;
-        int a[]=new int[100*100];
+        ArrayList<Integer> list=new ArrayList<>();
         for(int i=0;i<l-1;i+=2){
             while(nums[i]>0){
-                a[c++]=nums[i+1];
+                list.add(nums[i+1]);
                 nums[i]--;
             }
         }
-        int b[]=new int[c];
-        for(int i=0;i<c;i++)
-            b[i]=a[i];
-        return b;
+        int a[]=new int[list.size()];
+        for(int i=0;i<list.size();i++)
+            a[i]=list.get(i);
+        return a;
     }
 }
