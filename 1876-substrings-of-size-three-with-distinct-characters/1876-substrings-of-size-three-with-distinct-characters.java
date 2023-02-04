c2497bf770ec4cfebb530d907c9528fa4f6dc9fe
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-02-04 23:40:42 +0530

    Time: 2 ms (58.38%), Space: 41.9 MB (48.36%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -3,16 +3,18 @@ class Solution {
         int c=0;
         for(int i=0;i<s.length()-2;i++){
             int f[]=new int[26];
-            boolean flag=true;
+            int flag=3;
             f[s.charAt(i)-'a']+=1;
             f[s.charAt(i+1)-'a']++;
             f[s.charAt(i+2)-'a']++;
             for(int j=0;j<26;j++){
-                if(f[j]!=0&&f[j]!=1)
-                    flag=false;
+                if(f[j]==1)
+                    flag--;
+                if(flag==0){
+                    c++;
+                    break;
+                }
             }
-            if(flag==true)
-                c++;
         }
         return c;
     }
