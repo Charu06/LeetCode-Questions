12a1a9b85a3572279aad7c7c928eb7da56fc071b
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-02-06 23:35:51 +0530

    Time: 15 ms (30.58%), Space: 43.1 MB (28.04%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,23 +1,21 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        char c[]=new char[26];
+        HashMap<Character,Character> hs=new HashMap<>();
         char ch='a';
-        List<Character> list=new ArrayList<>();
-        for(int i=0;i<26;i++){
-            c[i]=ch;
-            ch+=1;
-        }
         for(int i=0;i<key.length();i++){
-            if((!list.contains(key.charAt(i)))&&(key.charAt(i)!=' '))
-                list.add(key.charAt(i));
+            if((!hs.containsKey(key.charAt(i)))&&(key.charAt(i)!=' ')){
+            hs.put(key.charAt(i),ch);
+            ch+=1;
+            }
         }
+        
         String s="";
         for(int i=0;i<message.length();i++){
             char m=message.charAt(i);
             if(m==' ')
                 s+=m;
             else
-                s+=c[list.indexOf(m)];
+                s+=hs.get(m);
         }
         return s;
     }
