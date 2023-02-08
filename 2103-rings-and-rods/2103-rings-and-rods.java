7e00be840e608523e369ee188fa4b00d486e0bc6
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-02-08 16:48:32 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (66.29%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,15 +1,20 @@
 class Solution {
     public int countPoints(String rings) {
-        String a[]=new String[10];
-        for(int i=0;i<10;i++)
-            a[i]="";
-        for(int i=0;i<rings.length()-1;i+=2){
-            a[rings.charAt(i+1)-48]+=rings.charAt(i);
-        }
+        boolean red[]=new boolean[10];
+        boolean green[]=new boolean[10];
+        boolean blue[]=new boolean[10];
         int r=0;
+        for(int i=0;i<rings.length();i+=2){
+            if(rings.charAt(i)=='R')
+                red[rings.charAt(i+1)-48]=true;
+            if(rings.charAt(i)=='G')
+                green[rings.charAt(i+1)-48]=true;
+            if(rings.charAt(i)=='B')
+                blue[rings.charAt(i+1)-48]=true;
+        }
         for(int i=0;i<10;i++){
-            if((a[i].indexOf("B")!=-1)&&(a[i].indexOf("R")!=-1)&&(a[i].indexOf("G")!=-1))
-                r++;
+            if(red[i]&&green[i]&&blue[i])
+              r++;
         }
         return r;
     }
