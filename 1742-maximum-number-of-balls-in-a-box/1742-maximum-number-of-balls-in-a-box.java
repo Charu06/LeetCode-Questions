85537401b501a6b6980be8a6cd8118977ede9660
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-02-09 22:45:10 +0530

    Time: 22 ms (93.09%), Space: 38.8 MB (97.78%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.java
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.java
@@ -2,18 +2,20 @@ class Solution {
     public int countBalls(int lowLimit, int highLimit) {
         int a[]=new int[46];
         for(int i=lowLimit;i<=highLimit;i++){
-            int sum=0;
-            int n=i;
-            while(n!=0){
-                sum+=n%10;
-                n/=10;
-            }
-            a[sum]++;
+            a[sod(i)]++;
         }
         int max=0;
         for(int i=1;i<46;i++){
             max=Math.max(max,a[i]);
         }
         return max;
+    }
+    public int sod(int n){
+        int sum=0;
+        while(n!=0){
+            sum+=n%10;
+            n/=10;
+            }
+        return sum;
     }
 }
