e9e94366b8e4c7c838e023b29536f8265ce29652
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-02-11 22:25:18 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (98.95%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -5,7 +5,7 @@ class Solution {
             f[nums[i]]++;
             }
         for(int i=0;i<501;i++){
-            if(f[i]%2!=0&&f[i]!=0)
+            if(f[i]%2!=0)
                 return false;
         }
         return true;
