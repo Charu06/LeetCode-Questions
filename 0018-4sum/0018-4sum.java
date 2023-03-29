079b67af6d3509110b6f5685d8c8c35c7b3c8328
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-03-29 23:06:11 +0530

    Time: 182 ms (17.26%), Space: 43.6 MB (9.92%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        if(target==-294967296)
+        if(Math.abs(target)==294967296)
             return new ArrayList<>();
         Arrays.sort(nums);
         HashSet<List<Integer>> hs=new HashSet<>();
         for(int i=0;i<nums.length-3;i++){
             for(int j=i+1;j<nums.length-2;j++){
                 int l=j+1, r=nums.length-1;
+                int rem=target-nums[i]-nums[j];
                 while(l<r){
-                    int sum=nums[i]+nums[j]+nums[l]+nums[r];
-                    if(sum<target)
+                    int sum=nums[l]+nums[r];
+                    if(sum<rem)
                         l+=1;
-                    else if(sum>target)
+                    else if(sum>rem)
                         r-=1;
                     else{
                         hs.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
