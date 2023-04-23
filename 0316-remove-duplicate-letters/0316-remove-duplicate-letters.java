f79d3d1cd30ecf350027d0d0425fe9cfb63e0985
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-04-24 00:49:43 +0530

    Time: 3 ms (68.49%), Space: 40.7 MB (99.77%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,21 +1,26 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        HashMap<Character, Integer> hm =new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            hm.put(s.charAt(i),i);
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            lastIndex[s.charAt(i) - 'a'] = i; // track the lastIndex of character presence
         }
-        ArrayDeque<Character> st=new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(!st.contains(ch)){
-                while(!st.isEmpty() && st.peek()>ch && hm.get(st.peek())>i)
-                    st.pop();
-                st.push(ch);
+        
+        boolean[] seen = new boolean[26]; // keep track seen
+        Stack<Integer> st = new Stack();
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (seen[curr]) continue; // if seen continue as we need to pick one char only
+            while (!st.isEmpty() && st.peek() > curr && i < lastIndex[st.peek()]){
+                seen[st.pop()] = false; // pop out and mark unseen
             }
+            st.push(curr); // add into stack
+            seen[curr] = true; // mark seen
         }
-        StringBuffer res=new StringBuffer();
-        for(char ch:st)
-            res.append(ch);
-        return res.reverse().toString();
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty())
+            sb.append((char) (st.pop() + 'a'));
+        return sb.reverse().toString();
     }
 }
