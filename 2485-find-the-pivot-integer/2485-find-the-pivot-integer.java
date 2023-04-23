5fb10b7d318a6f1d1040b123fbf083cb376fa318
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-04-23 22:47:52 +0530

    Time: 1 ms (83.97%), Space: 39.8 MB (44.96%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int s1=0, s2=(n*(n+1))/2;
+        int i=1;
+        while(i<=n){
+            s1=s1+i;
+            s2=s2-(i-1);
+            if(s1==s2)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+}
