c5a787c8d76129e499775f140eb6f5930f8b6519
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-04-23 23:29:50 +0530

    Time: 1 ms (83.97%), Space: 39.6 MB (75.06%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -2,7 +2,7 @@ class Solution {
     public int pivotInteger(int n) {
         int s1=0, s2=(n*(n+1))/2;
         int i=1;
-        while(i<=n){
+        while(s1<=s2){
             s1=s1+i;
             s2=s2-(i-1);
             if(s1==s2)
