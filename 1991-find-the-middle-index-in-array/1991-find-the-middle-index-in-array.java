58359a7af67bc40f630774f864f0615567731cf1
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-04-27 22:41:20 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (71.59%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -5,7 +5,6 @@ class Solution {
         int s1=0, s2=0, n=nums.length;
         for(int i=1;i<n;i++)
             s2+=nums[i];
-        System.out.println(s1+" "+s2);
         for(int i=1;i<=n;i++){
             if(s1==s2)
                 return i-1;
@@ -13,20 +12,7 @@ class Solution {
             s1+=nums[i-1];
             s2-=nums[i];
             }
-            System.out.println(s1+" "+s2);
         }
-        // s1=0;
-        // s2=0;
-        // for(int i=n-2;i>=0;i--)
-        //     s1+=nums[i];
-        // System.out.println(s1+" "+s2);
-        // for(int i=n-2;i>=0;i--){
-        //     if(s1==s2)
-        //         return i+1;
-        //     s1+=nums[i+1];
-        //     s2-=nums[i];
-        //     System.out.println(s1+" "+s2);
-        // }
         return -1; 
     }
 }
