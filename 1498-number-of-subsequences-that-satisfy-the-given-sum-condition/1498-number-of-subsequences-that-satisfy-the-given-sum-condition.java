e531b05385a8e4810ebef2c736db96301df7de5f
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-05-06 23:30:07 +0530

    Time: 33 ms (100.00%), Space: 50.8 MB (88.00%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -2,10 +2,11 @@ class Solution {
     public int numSubseq(int[] nums, int target) {
         Arrays.sort(nums);
         int mod=(int)1e9+7;
+        //memoization concept of dp
         int power[]=new int[nums.length];
         power[0]=1;
         for(int i=1;i<nums.length;i++)
-            power[i]=(power[i-1]*2)%mod;
+            power[i]=(power[i-1]*2)%mod; //power={1,2,4,8,16.....}
         int l=0, r=nums.length-1, res=0;
         while(l<=r){
             if(nums[l]+nums[r]<=target){
@@ -16,5 +17,12 @@ class Solution {
                 r--;
         }
         return res;
+        // int power (int x){
+        //     int result =1;
+        //     for(int i=0;i<x;i++)
+        //         result=(result*2)%mod;
+        //     return result;
+        // }
+        // We have not used this function because it takes more time so it will give TLE instead of that we have used memoization concept of DP
     }
 }
