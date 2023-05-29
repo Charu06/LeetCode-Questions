b33d959e5cc2df1891064a873a3ac1ff405fa98a
Charu Jain <83935950+Charu06@users.noreply.github.com>
2023-05-29 23:46:53 +0530

    Time: 3 ms (44.21%), Space: 43.9 MB (6.82%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,33 +10,26 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack s1=new Stack();
+        ArrayDeque<Integer> s1=new ArrayDeque<>();
         while(l1!=null){
             s1.push(l1.val);
             l1=l1.next;
         }
-        Stack s2=new Stack();
+        ArrayDeque<Integer> s2=new ArrayDeque<>();
         while(l2!=null){
             s2.push(l2.val);
             l2=l2.next;
         }
-        Stack res=new Stack();
         int c=0;
+        ListNode curr=null;
         while(!s1.isEmpty() || !s2.isEmpty() || c!=0){
-            int x=(!s1.isEmpty())?(int)s1.pop():0;
-            int y=(!s2.isEmpty())?(int)s2.pop():0;
+            int x=(!s1.isEmpty())?s1.pop():0;
+            int y=(!s2.isEmpty())?s2.pop():0;
             int sum=x+y+c;
             c=sum/10;
             sum=sum%10;
-            res.push(sum);
+            curr=new ListNode(sum, curr);
         }
-        ListNode dummy=new ListNode();
-        ListNode curr=dummy;
-        while(!res.isEmpty()){
-            ListNode n=new ListNode((int)res.pop());
-            curr.next=n;
-            curr=curr.next;
-        }
-        return dummy.next;
+        return curr;
     }
 }
