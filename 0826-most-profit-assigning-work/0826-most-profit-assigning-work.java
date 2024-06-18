b0a36607601818949535cdf1e77a016fac4a8086
Charu Jain <83935950+Charu06@users.noreply.github.com>
2024-06-18 14:30:29 +0530

    Time: 66 ms (23.09%), Space: 46.4 MB (7.74%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -5,15 +5,15 @@ class Solution {
         TreeMap<Integer, Integer> map=new TreeMap<>();
         map.put(0,0);
         for(int i=0;i<n;i++){
-            map.put(difficulty[i],Math.max(map.getOrDefault(difficulty[i],0),profit[i]));
+            map.put(difficulty[i],Math.max(map.getOrDefault(difficulty[i],0),profit[i])); // As there can be same difficulties for more than one tasks.
         }
         int best=0,res=0;
         for(int key: map.keySet()){
-            best=Math.max(best, map.get(key));
+            best=Math.max(best, map.get(key)); // finding out the best value of profit till 'key' difficulty
             map.put(key, best);
         }
         for(int i=0;i<m;i++){
-            res+=map.floorEntry(worker[i]).getValue();
+            res+=map.floorEntry(worker[i]).getValue(); //return a key-value mapping associated with the greatest key less than or equal to the given key, or null if there is no such key
         }
         return res;
     }
