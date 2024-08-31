745167788b535ac062cb4e6d83e28bd597257098
Charu Jain <83935950+Charu06@users.noreply.github.com>
2024-08-31 16:53:42 +0530

    Time: 1 ms (86.53%), Space: 43.4 MB (23.43%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res=new ArrayList<>();
+        solve(res, 0, 0, "", n);  
+        return res;
+    }
+    public void solve(List<String> res, int l, int r, String s, int n){
+        if(s.length()==n*2){
+            res.add(s);
+            return;
+        }
+        if(l<n){
+            solve(res, l+1, r, s+"(", n);
+        }
+        if(r<l){
+            solve(res, l, r+1, s+")", n);
+        }
+    }
+}
+/*
+The idea is to add ')' only after valid '('
+We use two integer variables left & right to see how many '(' & ')' are in the current string
+If left < n then we can add '(' to the current string
+If right < left then we can add ')' to the current string
+
+
+For n = 2, the recursion tree will be something like this,
+
+								   	(0, 0, '')
+								 	    |	
+									(1, 0, '(')  
+								   /           \
+							(2, 0, '((')      (1, 1, '()')
+							   /                 \
+						(2, 1, '(()')           (2, 1, '()(')
+						   /                       \
+					(2, 2, '(())')                (2, 2, '()()')
+						      |	                             |
+					res.append('(())')             res.append('()()')
+*/
