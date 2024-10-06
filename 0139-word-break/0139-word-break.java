47b52c65026489d205ed321d2bb62ee3ef637fce
Charu Jain <83935950+Charu06@users.noreply.github.com>
2024-10-06 15:48:59 +0530

    Time: 1 ms (99.57%), Space: 42.5 MB (71.32%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,68 @@
+class Node{
+    Node links[]=new Node[26];
+    boolean flag=false;
+    boolean containsKey(char ch){
+        return links[ch-'a']!=null;
+    }
+    void put(char ch, Node node){
+        links[ch-'a']=node;
+    }
+    Node get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+    boolean isEnd(){
+        return flag;
+    }
+}
+
+class Trie{
+    private static Node root;
+    Trie(){
+        root = new Node();
+    }
+    
+    public static void insert(String word){
+        Node node = root;
+        for(int i=0;i<word.length();i++){
+            if(!node.containsKey(word.charAt(i))){
+                node.put(word.charAt(i), new Node());
+            }
+            node=node.get(word.charAt(i));
+        }
+        node.setEnd();
+    }
+    
+    public static boolean canSegmentString(String s, int start, int[] memo){
+        if(start==s.length())
+            return true;
+        if(memo[start]!=-1) return memo[start]==1;
+        Node node=root;
+        for(int i=start;i<s.length();i++){
+            if(!node.containsKey(s.charAt(i))){
+                memo[start]=0;
+                return false;
+            }
+            node=node.get(s.charAt(i));
+            if(node.isEnd() && canSegmentString(s, i+1, memo)){
+                memo[start]=1;
+                return true;
+            }
+        }
+        memo[start]=0;
+        return false;
+    }
+}
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Trie obj=new Trie();
+        for(int i=0;i<wordDict.size();i++){
+            obj.insert(wordDict.get(i));
+        }
+        int memo[]=new int[s.length()];
+        Arrays.fill(memo, -1);
+        return obj.canSegmentString(s, 0, memo);
+    }
+}
