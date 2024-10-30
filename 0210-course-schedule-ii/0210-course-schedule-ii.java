922cb9c6b5ad7031cd11ca70051bb045015a1243
Charu Jain <83935950+Charu06@users.noreply.github.com>
2024-10-31 02:12:23 +0530

    Time: 5 ms (78.00%), Space: 45.7 MB (60.48%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,38 +1,38 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>();
         int n=prerequisites.length;
-        int ans[]=new int[numCourses];
-        int k=0;
+        int res[]=new int[numCourses];
+        Arrays.fill(res, -1);
+        int indegree[]=new int[numCourses];
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
-        }        
+        }
         for(int i=0;i<n;i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
-        }
-        int ind[]=new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            for(int x: adj.get(i)){
-                ind[x]++;
-            }
+            indegree[prerequisites[i][0]]++;
         }
         Queue<Integer> q=new LinkedList<>();
+        int j=0;
         for(int i=0;i<numCourses;i++){
-            if(ind[i]==0)
+            if(indegree[i]==0){
                 q.offer(i);
-        }
-        while(!q.isEmpty()){
-            int dep=q.poll();
-            ans[k++]=dep;
-            for(int x:adj.get(dep)){
-                ind[x]--;
-                if(ind[x]==0)
-                    q.offer(x);
             }
         }
-        if(k==numCourses)
-            return ans;
-        int[] arr={};
-        return arr;
+        while(!q.isEmpty()){
+            int x=q.peek();
+            res[j++]=q.poll();
+            for(int i:adj.get(x)){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.offer(i);
+                }
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(res[i]==-1)
+                return new int[]{};
+        }
+        return res;
     }
 }
